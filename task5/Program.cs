f557d7f48c87a56c644256b26d3c6a3f7ac44226
VALERIA <valeri.tereshenkova@icloud.com>
2023-04-22 12:35:46 +0300

    Task 12345

--- task5/Program.cs
+++ task5/Program.cs
@@ -4,7 +4,7 @@
 //  918 -> 8
 
 Console.WriteLine("Введите трехзначное число: ");
-int A = int.Parse(Console.ReadLine());
+int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(A % 10);
 if (A > 99 && A < 1000)
 {
